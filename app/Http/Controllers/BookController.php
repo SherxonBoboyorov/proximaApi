2455b4f8082553c0e5ec02971289d64d8fd643ae
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-08-19 21:16:13 +0500

    Intial commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Books;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Books::all();
+        return response()->json($books);
+    }
+
+
+    public function store(Request $request)
+    {
+        $book = new Books;
+
+        $book->task = $request->task;
+        $book->save();
+
+        return response()->json([
+            "message" => "Book added."
+        ], 201);
+    }
+
+
+
+    public function show($id) 
+    {
+        $book = Books::find($id);
+        if(!empty($book))
+        {
+            return response()->json($book);
+        }
+        else
+        {
+            return response()->json([
+                "message" => "Book not found"
+            ], 404);
+        }
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        if (Books::where('id', $id)->exists()) {
+            $book = Books::find($id);
+            $book->task = is_null($request->task) ? $book->task : $request->task;
+            $book->save();
+
+            return response()->json([
+                "message" => "Book updated."
+            ], 404);
+        }else{
+            return response()->json([
+                "message" => "Book not found."
+            ], 404);
+        }
+    }
+
+
+
+    public function destroy($id)
+    {
+        if (Books::where('id', $id)->exists()) {
+            $book = Books::find($id);
+            $book->delete();
+
+            return response()->json([
+                "message" => "records deleted."
+            ], 202);
+        }else{
+            return response()->json([
+                "message" => "Book not found"
+            ], 404);
+        }
+    }
+}
---
+++ database/seeders/BooksSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Books;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BooksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 50; $i++) {
+            Books::create([
+                'task' => $faker->task
+            ]);
+        }
+    }
+}
